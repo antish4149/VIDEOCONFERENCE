28576ce419fabc93e2d5e3c34430dc4c296a5300
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-07-01 00:46:03 +0530

    updated

--- backend/src/models/meetingModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const meetingSchema = new Schema({
-    user_id: {type:String, required: true},
-    meetingCode: {type: String, required:true},
-    date: {type: Date, default: Date.now, required: true}
-})
-
-const Meeting = mongoose.model("Meeting", meetingSchema);
