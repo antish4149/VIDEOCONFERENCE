4215ca9246c385e99d2bd6646a817a5c806411b6
Antish Kumar Yadav <antishyadav726@gmail.com>
2026-07-04 14:32:57 +0530

    Frontend: LangingPage component done

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,8 +1,40 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import '../App.css'
 
 export default function LandingPage(){
     return (
-        <div>Landing page</div>
+        <div className="landingPageContainer">
+            <nav>
+                <div className='navHeader'>
+                    <h2>MeetFriend</h2>
+                </div>
+                
+                <div className='navlist'>
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role="button">Login</div>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+
+                    <div role="button">
+                        <Link to={'/home'}>Get Started</Link>
+                    </div>
+
+                </div>
+
+                <div>
+                    <img src="/mobile.png"/>
+                </div>
+            </div>
+           
+        </div>
     )
 }
 // css to jsx
